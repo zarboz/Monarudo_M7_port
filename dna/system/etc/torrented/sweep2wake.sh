8688f153bb145af7e9733479faf80cd293b96569
Ryan <wallace.ryan10@gmail.com>
2014-03-02 00:16:45 -0500

    Update sweep2wake.sh

--- dna/system/etc/torrented/sweep2wake.sh
+++ dna/system/etc/torrented/sweep2wake.sh
@@ -12,7 +12,7 @@ do
     if [ -r $Fwhich ]
     then
         s2w=`cat $Fwhich`
-        for button in startbuton stopbutton
+        for button in startbuton endbutton
         do
             echo $s2w > "$S2W$button"
             echo "Setings $button button to $s2w"
