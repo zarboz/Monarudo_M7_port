7fcb25711ea47a4ad1189c4557c8531406df834d
Ryan <wallace.ryan10@gmail.com>
2014-03-02 00:11:22 -0500

    Update Script
    OLD
    =================
    #!/system/bin/sh
    
    Fstart=/sdcard/torrented-s2w-start
    Fend=/sdcard/torrented-s2w-end
    S2W=/sys/android_touch/sweep2wake
    
    echo "Enabling Sweep2Wake"
    echo 1 > /sys/android_touch/sweep2wake
    
    if [ -r $Fstart ]
    then
        s2w=`cat $Fstart`
        echo $s2w > "$S2W"_startbutton
        echo "Setings Start button to $s2w"
    else
        echo "Leaving Start button alone"
    fi
    
    if [ -r $Fend ]
    then
        s2w=`cat $Fend`
        echo $s2w > "$S2W"_endbutton
        echo "Setings End button to $s2w"
    else
        echo "Leaving End button alone"
    fi

--- dna/system/etc/torrented/sweep2wake.sh
+++ dna/system/etc/torrented/sweep2wake.sh
@@ -1,26 +1,23 @@
 #!/system/bin/sh
 
-Fstart=/sdcard/torrented-s2w-start
-Fend=/sdcard/torrented-s2w-end
-S2W=/sys/android_touch/sweep2wake
+F=/sdcard/torrented-s2w-
+S2W=/sys/android_touch/sweep2wake_
 
 echo "Enabling Sweep2Wake"
 echo 1 > /sys/android_touch/sweep2wake
 
-if [ -r $Fstart ]
-then
-    s2w=`cat $Fstart`
-    echo $s2w > "$S2W"_startbutton
-    echo "Setings Start button to $s2w"
-else
-    echo "Leaving Start button alone"
-fi
-
-if [ -r $Fend ]
-then
-    s2w=`cat $Fend`
-    echo $s2w > "$S2W"_endbutton
-    echo "Setings End button to $s2w"
-else
-    echo "Leaving End button alone"
-fi
+for which start stop
+do
+    Fwhich=$F"$which"
+    if [ -r $Fwhich ]
+    then
+        s2w=`cat $Fwhich`
+        for button in startbuton stopbutton
+        do
+            echo $s2w > "$S2W$button"
+            echo "Setings $button button to $s2w"
+        done
+    else
+        echo "Leaving $button button alone"
+    fi    
+done
