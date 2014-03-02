4a136aab6c679f845e5ded7c7cbc6f8d50452259
torrented <wallace.ryan10@gmail.com>
2014-03-02 17:27:12 -0500

    Updated s2w script again

--- dna/system/etc/torrented/sweep2wake.sh
+++ dna/system/etc/torrented/sweep2wake.sh
@@ -12,10 +12,10 @@ do
     if [ -r $Fwhich ]
     then
         s2w=`cat $Fwhich`
-        echo $s2w > "$S2W$which""button"
-        cur_btn=`cat $S2W$which"button"`
-        echo "Set $S2W$which""button to $cur_btn"
+        echo $s2w > "$S2W$which"button
+        cur_btn=`cat "$S2W$which"button`
+        echo Set "$S2W$which"button to $cur_btn
     else
-        echo "Leaving $S2W$which""button alone"
+        echo Leaving "$S2W$which"button alone
     fi    
 done
