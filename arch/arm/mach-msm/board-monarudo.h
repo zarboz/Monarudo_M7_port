8506ff6ad274e170b1cea3b67ddea699d65382c7
Bryan Smith <zarboz@gmail.com>
2014-03-11 22:30:17 -0600

    fix merge d3rP
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- arch/arm/mach-msm/board-monarudo.h
+++ arch/arm/mach-msm/board-monarudo.h
@@ -35,6 +35,7 @@
 
 int __init monarudo_init_keypad(void);
 /* cmdline_gpu variables */
+#ifdef CONFIG_CMDLINE_OPTIONS
 #define CMDLINE_3DGPU_DEFKHZ_0 500000000
 #define CMDLINE_3DGPU_DEFKHZ_1 500000000
 #define CMDLINE_2DGPU_DEFKHZ 266667000
